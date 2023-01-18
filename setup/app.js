e578a2f38e5decca56ea07c1deb5722369b3adea
Tuombe <jtuombe@gmail.com>
2023-01-18 12:49:32 +0200

    portfolio

--- setup/app.js
+++ setup/app.js
@@ -36,3 +36,28 @@ window.addEventListener("scroll", function () {
 });
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelector(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+
+    let position = element.offsetTop - navHeight;
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position - navHeight;
+    }
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    //close
+    linksContainer.style.height = 0;
+  });
+});
